34a32ade0b90ce5260c890c6d6c5c38a03675680
wensenl234 <wli9204@bths.edu>
2022-11-17 12:32:38 -0500

    dwdwddwddw

--- src/CafeSimulationRunner.java
+++ src/CafeSimulationRunner.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public class CafeSimulationRunner {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
-        System.out.println("What would you like the difficulty to be: ");
+        System.out.println("What would you like the difficulty to be? \n" +
+                "(1) Easy \n" +
+                "(2) Normal \n" +
+                "(3) Hard");
         CafeSimulation cafeSimulation = new CafeSimulation(Integer.parseInt(userInput.nextLine()));
         String panel = "===============";
         while(cafeSimulation.getBank() > 0) {
@@ -16,24 +19,24 @@ public class CafeSimulationRunner {
             System.out.println("Current bank: " + cafeSimulation.getBank() + "\n" + "Current rent: " + cafeSimulation.getRent());
             System.out.println(panel);
             System.out.println("What do you want to do now?" + "\n" +
-                    "(1) Buy products" + "\n" +
-                    "(2) Open store" + "\n" +
-                    "(3) Manage workers" + "\n" +
+                    "(1) Buy products\n" +
+                    "(2) Open store\n" +
+                    "(3) Manage workers\n" +
                     "(4) Change prices");
             int option = Integer.parseInt(userInput.nextLine());
             if(option == 1) {
                 System.out.println("What product would you like to buy? ");
                 String productBuy = userInput.nextLine();
                 if(cafeSimulation.getProducts().containsKey(productBuy)) {
-                    System.out.print("How many of " + productBuy + " would you like to buy?");
+                    System.out.print("How many of " + productBuy + " would you like to buy? ");
                     cafeSimulation.addProduct(productBuy, Integer.parseInt(userInput.nextLine()));
                 }
             } else if(option == 2) {
                 cafeSimulation.openStore();
             } else if(option == 3) {
-
+                System.out.println("Which position would you like to manage? ");
             } else if(option == 4) {
-
+                System.out.println("What price would you like to change? ");
             } else {
                 System.out.println("Not a valid choice.");
             }
